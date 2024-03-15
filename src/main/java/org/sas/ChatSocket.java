d9a5e601197b21e9290a720a363d82fb8fc64c09
Rafael Aguiar Silva <73291735+rafael-a-s@users.noreply.github.com>
2024-03-14 22:47:48 -0300

    Update ChatSocket.java

--- src/main/java/org/sas/ChatSocket.java
+++ src/main/java/org/sas/ChatSocket.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.websocket.OnClose;
 import jakarta.websocket.OnError;
 import jakarta.websocket.OnMessage;
@@ -13,6 +14,7 @@ import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 
 import org.jboss.logging.Logger;
+import org.sas.service.ChatService;
 
 @ServerEndpoint("/chat/{username}")
 @ApplicationScoped
@@ -20,10 +22,18 @@ public class ChatSocket {
 
     private static final Logger LOG = Logger.getLogger(ChatSocket.class);
 
+    @Inject
+    ChatService service;
+
     Map<String, Session> sessions = new ConcurrentHashMap<>();
 
     @OnOpen
     public void onOpen(Session session, @PathParam("username") String username) {
+        try {
+            service.registerUser(username);
+        } catch (Exception e) {
+           System.out.println(e);
+        }
         sessions.put(username, session);
     }
 
@@ -46,7 +56,18 @@ public class ChatSocket {
             broadcast("User " + username + " joined");
         } else {
             broadcast(">> " + username + ": " + message);
+            send(message, username);
         }
+    }
+
+    private void send(String message, String username) {
+        sessions.get(username)
+            .getAsyncRemote()
+            .sendObject(message, result -> {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
     }
 
     private void broadcast(String message) {
